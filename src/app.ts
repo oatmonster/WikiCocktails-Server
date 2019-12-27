495d84f0aca8e70521fb22e780ed201ea76b83e2
Alex Zhao <oatmonster@gmail.com>
2019-12-26 21:40:08 -0800

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use( cors() );
+
+// app.use( '/api', require( './api' ) );
+
+app.get( '/', ( req, res ) => res.send( 'Copyright Alex Zhao 2019' ) );
+
+exports.app = app;
