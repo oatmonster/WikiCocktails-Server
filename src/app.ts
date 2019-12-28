927abb1baa307d13433954f2cadaaf43e780e5c6
Alex Zhao <oatmonster@gmail.com>
2019-12-27 22:00:51 -0800

    Added some examples

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 
-const app = express();
+import * as routes from './api/routes'; // Get our route handlers
 
-app.use( cors() );
+const app = express(); // initialize the express server
 
-// app.use( '/api', require( './api' ) );
+app.use( cors() ); // Tell our server to use the cors middleware (don't worry about this)
 
+// We can define routes here but to keep things organized, we define them in ./routes/index.ts
 app.get( '/', ( req, res ) => res.send( 'Copyright Alex Zhao 2019' ) );
 
-exports.app = app;
+// Tell our server to send all routes starting with /api to the routes defined by routes.router
+// eg localhost:3000/api/test gets handled by the /test route defined in routes.router
+app.use( '/api', routes.router );
+
+export { app }
